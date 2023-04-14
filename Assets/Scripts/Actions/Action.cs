7486ba476521a881696866403427eeda32a287de
asfariabdu <asfariabdu@vuw.ac.nz>
2023-04-15 11:18:28 +1200

    Added basic action structure

--- Assets/Scripts/Actions/Action.cs
+++ Assets/Scripts/Actions/Action.cs
@@ -1,9 +1,40 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "Action")]
-public class Action : ScriptableObject{
+public abstract class Action : ScriptableObject{
     [SerializeField]
     private string actionName;
+
+    [SerializeField]
+    private TokenType tokenType;
+
+    [SerializeField]
+    private int tokenCost;
+
+    [SerializeField]
+    private bool isWhitelist;
+
+    [SerializeField]
+    private List<UnitID> unitList;
+
+    public virtual String CanExecuteAction(Unit unit, MapStateController msc){
+        return "TEMP"; // TODO: Add unit tokens so I can add a proper condition here.
+    }   
+
+    // Executes the action.
+    public abstract void ExecuteAction(Unit unit, MapStateController msc);
+    public abstract void EnhancedExecuteAction(Unit unit, MapStateController msc);
+
+    // Getters.
+    public abstract String GetActionDescription(Unit unit, MapStateController msc);
+    public abstract String GetEnhancedActionDescription(Unit unit, MapStateController msc);
+
+    // TODO: Move this later to where the units have their tokens.
+    private enum TokenType{
+        ACTION_TOKEN,
+        TRAVERSAL_TOKEN
+    }
 }
