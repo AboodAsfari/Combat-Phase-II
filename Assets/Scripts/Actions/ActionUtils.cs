594c84d6a03929ea74bf2d868900b38a7eb1f393
asfariabdu <asfariabdu@vuw.ac.nz>
2023-04-15 19:36:02 +1200

    Split traversal method into a more reusable method and added a get units method

--- Assets/Scripts/Actions/ActionUtils.cs
+++ Assets/Scripts/Actions/ActionUtils.cs
@@ -14,7 +14,7 @@ public class ActionUtils{
         Debug.Log("TEMP");
     }
 
-    public static HashSet<Vector2Int> GetTraversableTiles(Vector2Int initPos, int range){
+    private static HashSet<Vector2Int> BasicTileTraversal(Vector2Int initPos, int range, Func<Vector2Int, bool> addFilter, Func<Vector2Int, Vector2Int, bool> traverseFilter){
         HashSet<Vector2Int> traversableTiles = new HashSet<Vector2Int>();
         HashSet<Vector2Int> visitedTiles = new HashSet<Vector2Int>();
         Queue<Tuple<Vector2Int, int>> queue = new Queue<Tuple<Vector2Int, int>>();
@@ -25,7 +25,7 @@ public class ActionUtils{
         while(queue.Count > 0){
             Vector2Int pos = queue.Peek().Item1;
             int currRange = queue.Dequeue().Item2;
-            if(!pos.Equals(initPos) && currentController.msc.GetUnit(pos) == null) traversableTiles.Add(pos);
+            if(!pos.Equals(initPos) && addFilter(pos)) traversableTiles.Add(pos);
             if(currRange == range) continue;
 
             foreach(Direction dir in Enum.GetValues(typeof(Direction))){
@@ -34,12 +34,23 @@ public class ActionUtils{
                 if(visitedTiles.Contains(nextPos)) continue;
                 visitedTiles.Add(nextPos);
 
-                if(currentController.msc.GetTile(nextPos) != null && 
-                    (currentController.msc.GetUnit(nextPos) == null || currentController.msc.GetUnit(nextPos).GetUnitInfo().GetCanWalkThrough())) 
-                    queue.Enqueue(new Tuple<Vector2Int, int>(nextPos, currRange + 1));
+                if(traverseFilter(pos, nextPos)) queue.Enqueue(new Tuple<Vector2Int, int>(nextPos, currRange + 1));
             }
         }
-
         return traversableTiles;
+    }
+
+    public static HashSet<Vector2Int> GetTraversableTiles(Vector2Int initPos, int range){
+        return BasicTileTraversal(initPos, range, 
+            (pos) => currentController.msc.GetUnit(pos) == null, 
+            (pos, nextPos) => currentController.msc.GetTile(nextPos) != null && 
+                    (currentController.msc.GetUnit(nextPos) == null || currentController.msc.GetUnit(nextPos).GetUnitInfo().GetCanWalkThrough()));
+    }
+
+    public static HashSet<Vector2Int> GetNearbyUnits(Vector2Int initPos, int range){
+        return BasicTileTraversal(initPos, range, 
+            (pos) => currentController.msc.GetUnit(pos) != null,
+            (pos, nextPos) => currentController.msc.GetTile(nextPos) != null &&  (pos.Equals(initPos) || 
+                    currentController.msc.GetUnit(pos) == null || currentController.msc.GetUnit(pos).GetUnitInfo().GetCanWalkThrough()));
     }
 }
